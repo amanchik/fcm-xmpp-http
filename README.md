8b9569c416a8a04400a65f496aea608d2493e343
Anil <anil.xcode@gmail.com>
2019-05-20 11:38:38 +0530

    README

--- README.md
+++ README.md
@@ -1,1 +1,32 @@
 # fcm-xmpp-http
+Assuming the url https://www.example.com/get/app/keys returns a json array of the format 
+`[{"app_id":"fcm_sender_id_1","app_key":"fcm_server_key_1"},{"app_id":"fcm_sender_id_2","app_key":"fcm_server_key_2"}]
+`
+
+Run the following to get started.
+
+`docker build -t fcm-trial .`
+
+`export APP_URL=https://www.example.com/get/app/keys`
+
+`export REDIS_HOST=127.0.0.1`
+
+`docker stack deploy -c docker-compose.yml pushserver`
+
+Then you can do a post request to start sending push notifications.
+
+POST http://127.0.0.1:8768/fcm_sender_id
+`[{ "notification": {"title": "sample title","body": "new message 2","sound":"default"},"to" : "fcm_device_token","message_id":"123458846_478299338","priority":"high","delivery_receipt_requested":true}]
+`
+
+
+Run following to check notification status.
+
+For sent or failed status run.
+
+`redis> GET 20-05-2019_status_123458846_478299338`
+
+For delivery status run.
+
+`redis> GET 20-05-2019_message_123458846_478299338`
+
---
+++ apns.py
@@ -0,0 +1,116 @@
+"""
+Use HTTP/2 send APNS Example main reference
+===========================================
+http://gobiko.com/blog/token-based-authentication-http2-example-apns/
+Error 410 to detect uninstalls
+==============================
+https://leftshift.io/mobile-tracking-uninstalls-on-ios-and-android
+https://developer.apple.com/library/content/documentation/NetworkingInternet/Conceptual/RemoteNotificationsPG/CommunicatingwithAPNs.html#//apple_ref/doc/uid/TP40008194-CH11-SW1
+Expected result from below code:
+410
+{"reason":"Unregistered","timestamp":1496371306210}
+HTTP/2 push
+===========
+https://tpu.thinkpower.com.tw/tpu/File/html/201611/20161122001842_f.html?f=3dj6j8kd38895ksgtdddd93865jhr9sn3rqkh
+"""
+
+import json
+import jwt
+import time
+
+from hyper import HTTPConnection, HTTP20Connection
+
+ALGORITHM = 'ES256'
+
+APNS_KEY_ID = 'YOUR_KEY'
+
+# http://disq.us/p/1ecxfqv:
+# CMD: fold -w 64 APNsAuthKey_my_id.p8 > certs/APNsAuthKey_my_id_fold.p8
+APNS_AUTH_KEY = 'Your .p8 cert path'
+
+TEAM_ID = 'your_team_id'
+BUNDLE_ID = 'your_bundle_id'
+
+PUSH_ID = 'some_push_id'
+
+f = open(APNS_AUTH_KEY)
+secret = f.read()
+
+token = jwt.encode(
+    {
+        'iss': TEAM_ID,
+        'iat': time.time()
+    },
+    secret,
+    algorithm= ALGORITHM,
+    headers={
+        'alg': ALGORITHM,
+        'kid': APNS_KEY_ID
+    }
+)
+
+path = '/3/device/{0}'.format(PUSH_ID)
+
+request_headers = {
+    'apns-expiration': '0',
+    'apns-priority': '10',
+    'apns-topic': BUNDLE_ID,
+    'authorization': 'bearer {0}'.format(token.decode('ascii'))
+}
+
+# Open a connection the APNS server
+
+# https://github.com/genesluder/python-apns/pull/3
+# https://github.com/genesluder/python-apns/pull/3/commits/0f543b773c25b1a1d817f5f681912ed3c9c2ca35
+
+# Development
+conn = HTTP20Connection('api.development.push.apple.com:443', force_proto='h2')
+
+# Production
+# conn = HTTP20Connection('api.push.apple.com:443', force_proto='h2')
+
+payload_data = {
+    'aps': {
+        'alert' : 'All your base are belong to us.',
+		# This is silent push
+        'sound' : '',
+		# Key to silent push
+        'content-available': 1
+    }
+}
+payload = json.dumps(payload_data).encode('utf-8')
+
+# Send our request
+conn.request(
+    'POST',
+    path,
+    payload,
+    headers=request_headers
+)
+
+# https://github.com/genesluder/python-apns/pull/3
+# http://www.ehowstuff.com/how-to-install-and-update-openssl-on-centos-6-centos-7/
+resp = conn.get_response()
+print(resp.status)
+print(resp.read())
+
+# If we are sending multiple requests, use the same connection
+payload_data = {
+    'aps': {
+        'alert' : 'You have no chance to survive. Make your time.',
+        'sound' : '',
+        'content-available': 1
+    }
+}
+payload = json.dumps(payload_data).encode('utf-8')
+
+conn.request(
+    'POST',
+    path,
+    payload,
+    headers=request_headers
+)
+
+resp = conn.get_response()
+print(resp.status)
+print(resp.read())
