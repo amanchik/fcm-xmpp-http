860ed22da3fa104d92c8d7125c7b289c57f14774
Anil <anil.xcode@gmail.com>
2019-05-22 11:34:19 +0530

    reset

--- client.py
+++ client.py
@@ -5,18 +5,21 @@ from bs4 import BeautifulSoup
 import os
 import json
 import logging
+
 logging.basicConfig(format='%(asctime)s %(message)s', level=logging.INFO)
 log = logging.getLogger(__name__)
 import requests
 import datetime
 import time
+
 import asyncio
 from aiohttp import web
 import redis
+
 XMPP = {}
 app_keys = {}
-message_senders ={}
-failure_reasons ={'DEVICE_UNREGISTERED':1,'BAD_REGISTRATION':2}
+message_senders = {}
+failure_reasons = {'DEVICE_UNREGISTERED': 1, 'BAD_REGISTRATION': 2}
 r = redis.Redis(host=os.environ['REDIS_HOST'], port=6379, db=0)
 
 
@@ -25,7 +28,7 @@ class FCM(ClientXMPP):
     def __init__(self, sender_id, server_key):
         print(sender_id)
         print(server_key)
-        ClientXMPP.__init__(self, sender_id+'@fcm.googleapis.com', server_key)
+        ClientXMPP.__init__(self, sender_id + '@fcm.googleapis.com', server_key)
         self.default_port = 5235
         self.connected_future = asyncio.Future()
         self.add_event_handler("session_start", self.session_start)
@@ -38,31 +41,34 @@ class FCM(ClientXMPP):
         print("start callback")
         self.send_presence()
         self.get_roster()
+
     def message(self, msg):
         print("got message")
         if msg['type'] in ('chat', 'normal'):
             msg.reply("Thanks for sending\n%(body)s" % msg).send()
+
     def fcm_message(self, data):
-     #   print(data)
-        y = BeautifulSoup(str(data),features='html.parser')
-     #   print(y.message.gcm.text)
+        print(data)
+        y = BeautifulSoup(str(data), features='html.parser')
+        print(y.message.gcm.text)
         obj = json.loads(y.message.gcm.text)
 
-    #    print(obj)
+        print(obj)
         today = '{0:%d-%m-%Y}'.format(datetime.datetime.now())
         look_for = today + '_status_' + obj['message_id']
         if obj['message_type'] == 'ack':
-            r.set(look_for,json.dumps({'online_notification_sent_at':int(time.time()),'message_id':obj['message_id']}))
+            r.set(look_for,
+                  json.dumps({'online_notification_sent_at': int(time.time()), 'message_id': obj['message_id']}))
             if 'from' in obj:
-                ack = {'to':obj['from'],'message_id':obj['message_id'],'message_type':'ack'}
-                if obj['message_id'] in message_senders:
-                    XMPP[message_senders[obj['message_id']]].fcm_send(json.dumps(ack))
+                ack = {'to': obj['from'], 'message_id': obj['message_id'], 'message_type': 'ack'}
+                XMPP[message_senders[obj['message_id']]].fcm_send(json.dumps(ack))
         elif obj['message_type'] == 'nack':
-            op = {'online_notification_sent_at': int(time.time()), 'message_id': obj['message_id'],'error':obj['error']}
+            op = {'online_notification_sent_at': int(time.time()), 'message_id': obj['message_id'],
+                  'error': obj['error']}
             if obj['error'] in failure_reasons:
-                op['failure_reason']=failure_reasons[obj['error']]
+                op['failure_reason'] = failure_reasons[obj['error']]
             else:
-                op['failure_reason'] =3
+                op['failure_reason'] = 3
             r.set(look_for,
                   json.dumps(op))
         elif obj['message_type'] == 'receipt':
@@ -70,74 +76,48 @@ class FCM(ClientXMPP):
             r.set(look_for,
                   json.dumps({'notification_delivered_at': int(time.time()), 'message_id': obj['message_id'][4:]}))
 
-
     def start(self):
-        self.connect(address=('fcm-xmpp.googleapis.com',5235),use_ssl=True,disable_starttls=False)
-    def fcm_send(self,payload):
-        self.send_raw('<message><gcm xmlns="google:mobile:data">{0}</gcm></message>'.format(payload))
+        self.connect(address=('fcm-xmpp.googleapis.com', 5235), use_ssl=True, disable_starttls=False)
 
+    def fcm_send(self, payload):
+        self.send_raw('<message><gcm xmlns="google:mobile:data">{0}</gcm></message>'.format(payload))
 
     def reset_future(self):
         "Reset the future in case of disconnection"
         self.connected_future = asyncio.Future()
 
 
-async def run_job(request):
-    for fcm_sender_id in app_keys:
-        XMPP[fcm_sender_id] = FCM(fcm_sender_id, app_keys[fcm_sender_id])
-    # XMPP= FCM(os.environ['FCM_SENDER_ID'], os.environ['FCM_SERVER_KEY'])
-        XMPP[fcm_sender_id].start()
-    # XMPP.connect()
-        XMPP[fcm_sender_id].reset_future()
-
-    return web.Response(text="done")
-
 async def handle(request):
     "Handle the HTTP request and block until the vcard is fetched"
     err_404 = web.Response(status=404, text='Not found')
     body = await  request.json()
-    print("should send messages fo  r "+str(len(body)))
-  #  for message in body:
-   #     print(message['notification'])
+    #  for message in body:
+    #     print(message['notification'])
 
     fcm_sender_id = request.match_info.get('fcm_sender_id', "0")
-   # if not XMPP[fcm_sender_id].is_connected():
-   #     XMPP[fcm_sender_id].reconnect()
-    #count = 0
-    disconnected_end = False
+    if not XMPP[fcm_sender_id].is_connected():
+        XMPP[fcm_sender_id].reconnect()
     for message in body:
-        message_senders[message['message_id']] = fcm_sender_id
-        if XMPP[fcm_sender_id].is_connected():
+        print(message)
+        try:
+            message_senders[message['message_id']] = fcm_sender_id
             XMPP[fcm_sender_id].fcm_send(json.dumps(message))
-        else:
-            disconnected_end = True
-    if disconnected_end:
-        XMPP[fcm_sender_id] = FCM(fcm_sender_id, app_keys[fcm_sender_id])
-        XMPP[fcm_sender_id].start()
-        XMPP[fcm_sender_id].reset_future()
-
-     #   q.put({'id':fcm_sender_id,'message':message})
-        # count += 1
-        #try:
-         #   message_senders[message['message_id']] = fcm_sender_id
-        #    XMPP[fcm_sender_id].fcm_send(json.dumps(message))
-     #   except Exception as e:
-      #      print(e)
-       #     print("failed sending message for "+str(count))
-        #    return err_404
-
+        except Exception as e:
+            print(e)
+            return err_404
 
     return web.Response(text="done")
+
 
 async def init(loop, host: str, port: str):
     "Initialize the HTTP server"
     app = web.Application(loop=loop)
 
     app.router.add_route('POST', '/{fcm_sender_id}', handle)
-    app.router.add_route('GET', '/restart/connections', run_job)
     srv = await loop.create_server(app.make_handler(), host, port)
     log.info("Server started at http://%s:%s", host, port)
     return srv
+
 
 def main(namespace):
     "Start the xmpp client and delegate the main loop to asyncio"
@@ -147,21 +127,21 @@ def main(namespace):
     global XMPP
     loop.run_until_complete(init(loop, namespace.host, namespace.port))
 
-    for x  in data:
+    for x in data:
         app_keys[x['app_id']] = x['app_key']
-        XMPP[x['app_id']] = FCM(x['app_id'],x['app_key'])
-    # XMPP= FCM(os.environ['FCM_SENDER_ID'], os.environ['FCM_SERVER_KEY'])
+        XMPP[x['app_id']] = FCM(x['app_id'], x['app_key'])
+        # XMPP= FCM(os.environ['FCM_SENDER_ID'], os.environ['FCM_SERVER_KEY'])
         XMPP[x['app_id']].start()
-    #XMPP.connect()
+        # XMPP.connect()
         XMPP[x['app_id']].reset_future()
     for x in data:
         loop.run_until_complete(XMPP[x['app_id']].connected_future)
 
-    
     try:
         loop.run_forever()
     except KeyboardInterrupt:
         import sys
+
 
 def parse_args():
     "Parse the command-line arguments"
@@ -175,9 +155,9 @@ def parse_args():
 
     return parser.parse_args()
 
+
 HOST = '0.0.0.0'
 PORT = 8768
-
 
 if __name__ == "__main__":
     print(parse_args())
