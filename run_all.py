2e030595a91a9289d2f58ec74887333420c164f1
Anil <anil.xcode@gmail.com>
2019-07-10 14:18:51 +0530

    only works

--- run_all.py
+++ run_all.py
@@ -1,4 +1,5 @@
 import sys
+import redis
 import requests
 import os
 response = requests.get(os.environ['APP_URL'])
@@ -6,4 +7,9 @@ data = response.json()
 for x in data:
     sender_id = x['app_id']
     server_key = x['app_key']
-    os.system("python run.py "+sender_id+" "+server_key)
+    r = redis.Redis(host=os.environ['REDIS_HOST'], port=6379, db=0)
+    flen=r.llen(sender_id)
+    if flen>0:
+        os.system("python run.py "+sender_id+" "+server_key)
+    else:
+        print("no messages for "+str(flen))
