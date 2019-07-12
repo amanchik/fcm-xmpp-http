530d2f18dc623b1c7848151fcaf4d284527ae40d
Anil <anil.xcode@gmail.com>
2019-07-12 11:38:00 +0530

    more refined

--- run_all.py
+++ run_all.py
@@ -8,8 +8,10 @@ for x in data:
     sender_id = x['app_id']
     server_key = x['app_key']
     r = redis.Redis(host=os.environ['REDIS_HOST'], port=6379, db=0)
-    flen=r.llen(sender_id)
-    if flen>0:
-        os.system("python run.py "+sender_id+" "+server_key)
-    else:
-        print("no messages for "+str(sender_id))
+    while True:
+        flen=r.llen(sender_id)
+        if flen>0:
+            os.system("python run.py "+sender_id+" "+server_key)
+        else:
+            print("no messages for "+str(sender_id))
+            break
