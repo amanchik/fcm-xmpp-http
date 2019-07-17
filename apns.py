4c5746e31a3dfcb7ffcaef38a5d7a34f2aa20207
Anil <anil.xcode@gmail.com>
2019-07-17 12:02:15 +0530

    include ios as well

--- apns.py
+++ apns.py
@@ -17,21 +17,21 @@ https://tpu.thinkpower.com.tw/tpu/File/html/201611/20161122001842_f.html?f=3dj6j
 import json
 import jwt
 import time
-
+import sys
 from hyper import HTTPConnection, HTTP20Connection
 
 ALGORITHM = 'ES256'
 
-APNS_KEY_ID = 'YOUR_KEY'
+APNS_KEY_ID = sys.argv[1]
 
 # http://disq.us/p/1ecxfqv:
 # CMD: fold -w 64 APNsAuthKey_my_id.p8 > certs/APNsAuthKey_my_id_fold.p8
-APNS_AUTH_KEY = 'Your .p8 cert path'
+APNS_AUTH_KEY = sys.argv[2]
 
-TEAM_ID = 'your_team_id'
-BUNDLE_ID = 'your_bundle_id'
+TEAM_ID = sys.argv[3]
+BUNDLE_ID = sys.argv[4]
 
-PUSH_ID = 'some_push_id'
+PUSH_ID = sys.argv[5]
 
 f = open(APNS_AUTH_KEY)
 secret = f.read()
@@ -64,14 +64,14 @@ request_headers = {
 # https://github.com/genesluder/python-apns/pull/3/commits/0f543b773c25b1a1d817f5f681912ed3c9c2ca35
 
 # Development
-conn = HTTP20Connection('api.development.push.apple.com:443', force_proto='h2')
+#conn = HTTP20Connection('api.development.push.apple.com:443', force_proto='h2')
 
 # Production
-# conn = HTTP20Connection('api.push.apple.com:443', force_proto='h2')
+conn = HTTP20Connection('api.push.apple.com:443', force_proto='h2')
 
 payload_data = {
     'aps': {
-        'alert' : 'All your base are belong to us.',
+        'alert' : 'Are you correcting the test?',
 		# This is silent push
         'sound' : '',
 		# Key to silent push
